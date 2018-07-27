ab1013c22c4c4e210e8cb2aafc83c316a2b7c45d
Faridmeha <41186045+Faridmeha@users.noreply.github.com>
2018-07-28 00:01:59 +0200

    Add files via upload

---
+++ pitaya/pitaya.js
@@ -0,0 +1,46 @@
+
+					
+
+function slideUp(el) {
+  var elem = document.getElementById(el);
+  elem.style.transition = "all 2s ease-in-out";
+  elem.style.height = "0px";
+}
+function slideDown(el) {
+  var elem = document.getElementById(el);
+  elem.style.transition = "all 2s ease-in-out";
+  elem.style.height = "400px";
+}
+
+
+
+
+
+
+// <div class="container-fluid col-md-12">
+// 	<div class="text-row col-md-6">
+// 		<div class="text-row col-md-3">
+// 		<center> <h5>VENIR CHEZ PITAYA</h5></center>
+//     <i><ion-icon name="mail"></ion-icon><br>9 rue des Marronniers<br>69002 Lyon</i></div>	
+// 		</div>
+// 		<div class="text-row col-md-3">
+// 		<div> <i><ion-icon name="time"></ion-icon><br><ul>Lundi – jeudi<br>
+//       		12:00-14:30<br>
+//       		19:00-22:30<br>
+//       		Vendredi – samedi<br>
+//       		12:00-14:30<br>
+//       		19:00-23:00<br>
+//       		Dimanche<br>
+//       		19:00-22:30</ul><br>
+//          	</i>
+   	
+// 		</div>
+// 		<div class="footer">
+// 		 <a href="#" class="btn btn-primary">OBTENIR L'ITINERAIRE</a>	
+// 		</div>
+// 	</div>
+// 	<div class="row-img col-md-4">
+// 		<img class="googlemap" src="image/googlemap.png">	
+// 	</div>
+// </div>
+// 					
